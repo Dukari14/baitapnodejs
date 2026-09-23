de37f68b9d1409aad00617a24e4c35da3ae35b8a
Dukari14 <dovietdung44@gmail.com>
2026-09-23 02:14:55 +0000

    Change readme

---
+++ BanHang.sql
@@ -0,0 +1,44 @@
+CREATE DATABASE BanHang;
+USE BanHang;
+
+CREATE TABLE PRODUCTS (
+	prod_id INT(10) NOT NULL,
+	prod_name VARCHAR(255) NOT NULL,
+    prod_price INT(15) NOT NULL DEFAULT 0,
+	prod_quantity INT(15) NOT NULL DEFAULT 0,
+    prod_created_at timestamp NULL DEFAULT NULL,
+    pi_id int(10) NOT NULL,
+    cate_id int(10) NOT NULL,
+	PRIMARY KEY (prod_id)
+);
+
+CREATE TABLE CATEGORY (
+	cate_id int(10) NOT NULL,
+	cate_name varchar(255) NOT NULL,
+	cate_variable varchar(255) NOT NULL,
+	cate_created_at timestamp NULL DEFAULT NULL,
+	cate_parent int(11) NOT NULL DEFAULT 0,
+	PRIMARY KEY (cate_id)
+);
+
+CREATE TABLE PRODUCTIMAGE (
+	pi_id int(10) NOT NULL,
+	prod_id INT(10) NOT NULL,
+	prod_image varchar(255) DEFAULT NULL,
+	pi_created_at timestamp NULL DEFAULT NULL,
+	PRIMARY KEY (pi_id)
+);
+
+CREATE TABLE USERS (
+	user_id int(10) NOT NULL,
+	user_name varchar(255) NOT NULL,
+	user_email varchar(255) NOT NULL,
+	user_phone char(255) DEFAULT NULL,
+	user_address varchar(255) DEFAULT NULL,
+	user_password varchar(255) NOT NULL,
+	user_remember_token varchar(100) DEFAULT NULL,
+	user_created_at timestamp NULL DEFAULT NULL,
+	PRIMARY KEY (user_id)
+);
+
+
